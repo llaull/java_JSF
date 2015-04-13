6d0b359e2f50451ee2bbc954b2429bd7de000a4e
llaull <laurent@llovem.eu>
2015-04-13 10:36:49 +0200

    Signed-off-by: llaull <laurent@llovem.eu>

---
+++ src/java/beans/Joueur.java
@@ -0,0 +1,56 @@
+package beans;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@RequestScoped
+@Named
+public class Joueur {
+    
+    private String login;
+    private int score;
+    private String email;
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the score
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * @param score the score to set
+     */
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
